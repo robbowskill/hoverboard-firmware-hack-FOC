28c20e68f0f0125d24c1f183e49c9346be49ff5a
robbowskill <149394131+robbowskill@users.noreply.github.com>
2023-11-11 18:05:43 +0000

    updated speed and steering coeficient

--- Inc/config.h
+++ Inc/config.h
@@ -367,8 +367,8 @@
 
   // # maybe good for ARMCHAIR #
   #define FILTER                  3276    //  0.05f
-  #define SPEED_COEFFICIENT       8192    //  0.5f
-  #define STEER_COEFFICIENT       62259   // -0.2f
+  #define SPEED_COEFFICIENT       6000    //  0.5f 
+  #define STEER_COEFFICIENT       29462   // -0.2f
   // #define SUPPORT_BUTTONS                 // Define for Nunchuk buttons support
 #endif
 // ############################# END OF VARIANT_NUNCHUK SETTINGS #########################
